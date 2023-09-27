d9be8d03b8af552ab0e51aea5bb4be989111495f
Pavlo Reutskiy <pavel.reutskiy@gmail.com>
2023-09-27 19:32:52 +0300

    implement nav hover animation

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -18,6 +18,7 @@ export const Navbar = (): JSX.Element => {
     setIsOpen(prevState => !prevState);
   };
 
+  // #region Sidebar animation
   useEffect(() => {
     if (isOpen) {
       gsap.to('.bar1', { duration: .3, width: '32px', height: '3px', top: '50%', rotate: '45deg' });
@@ -57,13 +58,31 @@ export const Navbar = (): JSX.Element => {
         <div className="hidden md:flex md:items-center md:justify-end md:min-w-[45%]">
           <ul className="flex space-x-6 mr-9 text-lg font-bold text-black uppercase leading-normal">
             <li role="menuitem">
-              <Link href="/">Home</Link>
+              <Link
+                href="/"
+                data-link-alt="Home"
+                className="link"
+              >
+                <span className="text">Home</span>
+              </Link>
             </li>
             <li role="menuitem">
-              <Link href="/about">About</Link>
+              <Link
+                href="/about"
+                data-link-alt="About"
+                className="link"
+              >
+                <span className="text">About</span>
+              </Link>
             </li>
             <li role="menuitem">
-              <Link href="/contact">Contact</Link>
+              <Link
+                href="/contact"
+                data-link-alt="Contact"
+                className="link"
+              >
+                <span className="text">Contact</span>
+              </Link>
             </li>
           </ul>
 
@@ -79,7 +98,13 @@ export const Navbar = (): JSX.Element => {
             </div>
 
             <div className="text-lg font-bold text-black uppercase leading-normal">
-              <Link href="/subscribe">Subscribe</Link>
+              <Link
+                href="/subscribe"
+                data-link-alt="Subscribe"
+                className="link"
+              >
+                <span className="text">Subscribe</span>
+              </Link>
             </div>
           </div>
         </div>
@@ -89,9 +114,9 @@ export const Navbar = (): JSX.Element => {
           className="w-7 h-7 relative z-20 md:hidden"
           onClick={toggleMenu}
         >
-          <span className="bar1 top-[25%] menuToggleStyles"></span>
-          <span className="bar2 top-[50%] menuToggleStyles"></span>
-          <span className="bar3 top-[75%] menuToggleStyles"></span>
+          <span className="bar1 top-[25%] menu-toggle-styles"></span>
+          <span className="bar2 top-[50%] menu-toggle-styles"></span>
+          <span className="bar3 top-[75%] menu-toggle-styles"></span>
         </div>
       </div>
 

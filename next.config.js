136c057064c34f35789b2448b3f4e93f72e09a80
Pavlo Reutskiy <pavel.reutskiy@gmail.com>
2024-01-24 08:40:20 +0200

    start implement blog article page2

--- next.config.js
+++ next.config.js
@@ -2,11 +2,11 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '1337',
-      },
+      // {
+      //   protocol: 'http',
+      //   hostname: '127.0.0.1',
+      //   port: '1337',
+      // },
       {
         protocol: 'https',
         hostname: 'thank-code-strapi-84mp2.ondigitalocean.app',

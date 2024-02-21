7f33bb1c81c017be4031cb49f18bd252010c6ffa
PavloReutskiy <87998640+PavloReutskiy@users.noreply.github.com>
2024-02-21 08:52:39 +0200

    fix config for DO spaces

--- next.config.js
+++ next.config.js
@@ -1,18 +1,23 @@
 /** @type {import('next').NextConfig} */
+const isLocal = process.env.NODE_ENV === 'development';
+
 const nextConfig = {
   images: {
-    remotePatterns: [
-      // {
-      //   protocol: 'http',
-      //   hostname: '127.0.0.1',
-      //   port: '1337',
-      // },
-      {
-        protocol: 'https',
-        hostname: 'thank-code-strapi-84mp2.ondigitalocean.app',
-      },
-    ],
-    domains: ['thank-code-strapi-84mp2.ondigitalocean.app', '127.0.0.1:1337'],
+    remotePatterns: isLocal
+      ? [
+        {
+          protocol: 'http',
+          hostname: '127.0.0.1',
+          port: '1337',
+        },
+      ]
+      : [
+        {
+          protocol: 'https',
+          hostname: 'thank-code-its-friday-space.fra1.digitaloceanspaces.com',
+        },
+      ],
+    domains: ['thank-code-its-friday-space.fra1.digitaloceanspaces.com', '127.0.0.1:1337'],
   },
 };
 
